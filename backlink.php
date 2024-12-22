d2af6c90de32f64b5cdbe949454fc961c113f4ba
feliciacan118 <feliciacan118@gmail.com>
2024-12-22 08:37:49 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -1,4 +1,5 @@
 <div style="display:none">
 <a href="https://penerbit.umk.ac.id/janda/">https://penerbit.umk.ac.id/janda/</a>
 <a href="https://vote.smkn1sikur.sch.id/">https://vote.smkn1sikur.sch.id/</a>
+<a href="https://eemisas.aknpacitan.ac.id/dpr/">https://eemisas.aknpacitan.ac.id/dpr/</a>
 </div>
