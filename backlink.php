4c1ede0637490716f3963082bc8ba741766c4364
feliciacan118 <feliciacan118@gmail.com>
2024-12-22 22:09:02 +0700

    Update backlink.php

--- backlink.php
+++ backlink.php
@@ -3,4 +3,5 @@
 <a href="https://vote.smkn1sikur.sch.id/">https://vote.smkn1sikur.sch.id/</a>
 <a href="https://eemisas.aknpacitan.ac.id/dpr/">https://eemisas.aknpacitan.ac.id/dpr/</a>
 <a href="https://krs.stit-hasiba.ac.id/janda/">https://krs.stit-hasiba.ac.id/janda/</a>
+<a href="https://journal.unuha.ac.id/janda/">https://journal.unuha.ac.id/janda/</a>
 </div>
